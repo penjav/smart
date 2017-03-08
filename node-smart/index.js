306e959802045c1a83b70fc5ec0ab705a50f709c
penpagyaltsen <penpagyaltsen@192.168.0.13>
2017-03-07 20:09:24 -0500

    first

---
+++ node-smart/index.js
@@ -0,0 +1,9 @@
+/**
+ * http://usejsdoc.org/
+ */
+var http = require('http');
+http.createServer(function handler(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hi this is updated information. Please go ahead.");
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
